da4824ab8b33cf67b46f014e004d77e3a1d1c655
DiegoSencico <48455278@sencico.edu.pe>
2023-12-14 12:01:29 -0500

    se creo una funcion con un mesaje de bienvenidad a los usuarios

---
+++ codigo20.js
@@ -0,0 +1,3 @@
+function Bienvenidos(){
+    console.log("Bienvenido ciudadano este es una empresa del La Municipalidad del Rimac con los precios mas bajos para que viajes por todo el peru si que gastes mucho");
+}
